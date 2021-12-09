8f6d35910e56ee47d58bb6f4a82e369d8db8d62a
isokolov <illya.sokolov82@gmail.com>
2021-12-09 21:27:35 +0200

    Initial commit

---
+++ src/main/java/ru/javabegin/backend/todo/TodoBackendApplication.java
@@ -0,0 +1,15 @@
+package ru.javabegin.backend.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class TodoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoBackendApplication.class, args);
+    }
+
+}
